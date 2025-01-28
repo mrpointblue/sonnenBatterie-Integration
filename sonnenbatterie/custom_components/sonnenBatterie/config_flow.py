e6cf509f30aade4dcc5494e8ac4f5a7b05a99508
mrpointblue <mrpointblue@aol.com>
2025-01-28 19:41:14 +0100

    Fixes an error where the polling interval was not adopted

--- sonnenbatterie/custom_components/sonnenBatterie/config_flow.py
+++ sonnenbatterie/custom_components/sonnenBatterie/config_flow.py
@@ -2,50 +2,41 @@ from homeassistant import config_entries
 from homeassistant.core import callback
 import voluptuous as vol
 from homeassistant.helpers import config_validation as cv
-from .const import DOMAIN
 from .const import DEFAULT_PREFIX
 
-class SonnenBatterieConfigFlow(config_entries.ConfigFlow, domain="sonnenbatterie"):
-    """Handle a config flow for SonnenBatterie."""
+class SonnenOptionsFlow(config_entries.OptionsFlow):
+    """Handle options flow for the SonnenBatterie integration."""
 
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        errors = {}
+    def __init__(self, config_entry):
+        """Initialize the options flow."""
+        self.entry_id = config_entry.entry_id
+        self.data = config_entry.data
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
         if user_input is not None:
-            ip = user_input.get("ip_address")
-            token = user_input.get("token")
-            scan_interval = user_input.get("scan_interval")
-            custom_prefix = user_input.get("custom_prefix", DEFAULT_PREFIX)
-
-            if not ip or not token:
-                errors["base"] = "invalid_credentials"
-            elif scan_interval < 5:
-                errors["scan_interval"] = "too_short"
-            else:
-                return self.async_create_entry(
-                    title="SonnenBatterie",
-                    data={
-                        "ip_address": ip,
-                        "token": token,
-                        "scan_interval": scan_interval,
-                        "custom_prefix": custom_prefix,
-                    },
-                )
+            self.hass.config_entries.async_update_entry(
+                self.hass.config_entries.async_get_entry(self.entry_id),
+                data={
+                    "ip_address": user_input["ip_address"],
+                    "token": user_input["token"],
+                    "scan_interval": user_input["scan_interval"],
+                    "custom_prefix": user_input.get("custom_prefix", DEFAULT_PREFIX),
+                },
+            )
+            return self.async_create_entry(title="", data=user_input)
 
         schema = vol.Schema(
             {
-                vol.Required("ip_address"): cv.string,
-                vol.Required("token"): cv.string,
-                vol.Required("scan_interval", default=30): vol.All(vol.Coerce(int), vol.Range(min=5)),
-                vol.Optional("custom_prefix", default=DEFAULT_PREFIX): cv.string,
+                vol.Required("ip_address", default=self.data["ip_address"]): cv.string,
+                vol.Required("token", default=self.data["token"]): cv.string,
+                vol.Required(
+                    "scan_interval", default=self.data.get("scan_interval", 30)
+                ): vol.All(vol.Coerce(int), vol.Range(min=5)),
+                vol.Optional(
+                    "custom_prefix", default=self.data.get("custom_prefix", DEFAULT_PREFIX)
+                ): cv.string,
             }
         )
 
-        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        """Return the options flow handler."""
-        from .options_flow import SonnenOptionsFlow
-        return SonnenOptionsFlow(config_entry)
+        return self.async_show_form(step_id="init", data_schema=schema)
